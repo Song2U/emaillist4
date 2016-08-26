1bdbc75d0b46894dc35002f609dabafae0ceb66b
S401-07 <S401-07@S401-07>
2016-08-26 15:49:25 +0900

    20160826 Spring 이용한 emailList 구현

---
+++ EmailList4/src/main/java/kr/ac/sungkyul/emaillist/dao/EmailListDao.java
@@ -0,0 +1,105 @@
+package kr.ac.sungkyul.emaillist.dao;
+
+import java.sql.*;
+import java.util.*;
+
+import org.springframework.stereotype.Repository;
+
+import kr.ac.sungkyul.emaillist.vo.EmailListVo;
+
+@Repository
+public class EmailListDao {
+	private Connection getConnection() throws SQLException{
+		Connection conn = null;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} 
+		return conn;
+	}
+
+	public boolean insert(EmailListVo vo) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int count = 0;
+
+		try {
+			conn = getConnection();
+
+			String sql = "insert into EMAILLIST values(seq_emaillist.nextval, ?, ?, ?, sysdate)";
+
+			pstmt = conn.prepareCall(sql);
+			pstmt.setString(1, vo.getLastName());
+			pstmt.setString(2, vo.getFirstName());
+			pstmt.setString(3, vo.getEmail());
+
+			count = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		} finally {
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return (count == 1);
+	}
+
+	public List<EmailListVo> getList() {
+		List<EmailListVo> list = new ArrayList<EmailListVo>();
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+
+		try {
+			conn = getConnection();
+			stmt = conn.createStatement();
+
+			String sql = "select no, first_name, last_name, email, to_char(REG_DATE, 'yyyy-mm-dd') from EMAILLIST order by REG_DATE desc";
+			rs = stmt.executeQuery(sql);
+
+			while (rs.next()) {
+				Long no = rs.getLong(1);
+				String firstName = rs.getString(2);
+				String lastName = rs.getString(3);
+				String email = rs.getString(4);
+				String regDate = rs.getString(5);
+
+				EmailListVo vo = new EmailListVo();
+				vo.setNo(no);
+				vo.setFirstName(firstName);
+				vo.setLastName(lastName);
+				vo.setEmail(email);
+				vo.setRegDate(regDate);
+				list.add(vo);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (stmt != null) {
+					stmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return list;
+	}
+}
